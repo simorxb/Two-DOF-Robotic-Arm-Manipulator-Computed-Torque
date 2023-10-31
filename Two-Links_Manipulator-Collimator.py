6f5251b5025a004b7c370969b774e73e7e53f957
Simone <bertoni.simone@gmail.com>
2023-10-31 13:31:09 +0000

    Changes to plot computed torque example

--- Two-Links_Manipulator-Collimator.py
+++ Two-Links_Manipulator-Collimator.py
@@ -9,23 +9,28 @@ token = token_file.read()
 # Load model from Collimator
 project_uuid = "221181d1-4494-4cf8-a58a-61345a7aa14c"
 collimator.set_auth_token(token, project_uuid)
-model = collimator.load_model("Two Links Manipulator Simulation")
+model = collimator.load_model("Two Links Manipulator - Computed Torque")
 
 # Simulate model
 sim = collimator.run_simulation(model)
 res = sim.results.to_pandas()
 
+print("Keys in 'res':", res.keys())
+
+
 # Create figure
 plt.figure()
-plt.title("Two Links Manipulator Simulation")
+plt.title("Two Links Manipulator - Computed Torque")
 
 # Plot joint angles response
 plt.subplot(2, 1, 1)
 
 plt.plot(res.index, res["Two_Links_Manipulator.theta1"]*180/math.pi, label=r"$\theta_1$")
 plt.plot(res.index, res["Two_Links_Manipulator.theta2"]*180/math.pi, label=r"$\theta_2$")
+plt.plot(res.index, res["First_Order_Filter_0.out_0"]*180/math.pi, label=r"$\theta_1$ Setpoint", linestyle='dashed')
+plt.plot(res.index, res["First_Order_Filter_2.out_0"]*180/math.pi, label=r"$\theta_2$ Setpoint", linestyle='dashed')
 
-plt.xlabel("Time [s]")
+
 plt.ylabel("Joint Angle [deg]")
 plt.legend()
 plt.grid()
@@ -33,8 +38,8 @@ plt.grid()
 # Plot controller output
 plt.subplot(2, 1, 2)
 
-plt.plot(res.index, res["Step_0.out_0"], label=r"$\tau_1$")
-plt.plot(res.index, res["Step_1.out_0"], label=r"$\tau_2$")
+plt.plot(res.index, res["PythonScript_ComputedTorque.tau1"], label=r"$\tau_1$")
+plt.plot(res.index, res["PythonScript_ComputedTorque.tau2"], label=r"$\tau_2$")
 plt.xlabel("Time [s]")
 plt.ylabel("Torque [Nm]")
 plt.legend()
